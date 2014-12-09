43cd9adc9479648ee9cdbf5ff551e7c72ab0356b
Zach Wright <rzwright484@gmail.com>
2014-12-09 01:02:03 -0800

    Fix flexbox column for mobile

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ $('#comments').hide();
 
 $('#commentsButton').click(function(){
 	$('#comments').show();
-	$('#buttons').css('margin-bottom', '2.5%')
+	$('#buttons').css('margin-bottom', '3%');
+	$('#commentsButton').attr('value','Hide Comments');
 });
 
+
 $('#getStartedButton').click(function(){
-$('#intro').fadeOut("slow");
+	$('#intro').fadeOut("slow");
 });
 
 $('#submitButton').click(function(){
-$('#submitInput').val('');
-alert("thanks for your submission");
+	$('#submitInput').val('');
+	alert("thanks for your submission");
+
 })
 
 $('#previousButton').click(function(){
-$('#previuosContents').append('<p>Why would there be comments here?</p>');
+	$('#previuosContents').append('<p>Why would there be comments here?</p>');
 })
